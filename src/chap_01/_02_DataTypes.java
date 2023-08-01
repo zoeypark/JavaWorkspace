3d7af63ac0842d2597903336ee7e99b015e134f6
박주연 <zoe@bagjuyeon-ui-MacBookPro.local>
2023-08-01 23:38:29 +0900

    java-day1

---
+++ src/chap_01/_02_DataTypes.java
@@ -0,0 +1,18 @@
+package chap_01;
+
+public class _02_DataTypes {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("안녕하세요?");
+        System.out.println(12);
+        System.out.println(-34);
+        System.out.println(3.14);
+        System.out.println(true);
+        System.out.println(false);
+
+        System.out.println(123 + 345);
+
+        System.out.println("public static void main은 psvm으로 줄여서 쓸 수 있습니다.");
+        System.out.println("System.out.println()은 sout로 줄여서 쓸 수 있습니다.");
+    }
+}
